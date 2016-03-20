5988e050da3799174fe74e1feeb2450ac0ee9755
shaskaj1 <jlshaska@gmail.com>
2016-03-20 12:10:16 -0400

    JS project stuff

--- hw3/q1/src/hw3q1/listeners/ContextListener.java
+++ hw3/q1/src/hw3q1/listeners/ContextListener.java
@@ -1,6 +1,7 @@
 package hw3q1.listeners;
 
 import hw3q1.model.domain.dao.CrappyUserDAO;
+import hw3q1.model.domain.dao.ProjectDAO;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -23,6 +24,7 @@ public class ContextListener implements ServletContextListener {
         // Create userDAO w/ DB connection
         // Save DB enabled user dao
         context.getServletContext().setAttribute("userdao", new CrappyUserDAO());
+        context.getServletContext().setAttribute("projectdao", new ProjectDAO());
         LOGGER.info("Context Intialized");
     }
 
